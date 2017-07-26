f22674c7e7b9793cf77866e7bdb8158e0b304595
Kees Postma <kk.postma@gmail.com>
2017-07-26 21:14:34 +0100

    Allows user to create appointments. Renders the appointments list instead of reloading page

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,17 @@
 class AppointmentsController < ApplicationController
   def index
+    @appointment = Appointment.new
+    @appointments = Appointment.order('created_at DESC')
+  end
+
+  def create
+    @appointment = Appointment.create(appointment_params)
+    @appointments = Appointment.order('created_at DESC')
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:title, :appointment_time)
   end
 end
